96fbdb23ac625229d2defc94c645ab5605486a2c
Elad <eladbendavid7@gmail.com>
2023-06-06 00:41:39 +0300

    Added cropped image logic

--- src/Components/App/components/CroppedImage.js
+++ src/Components/App/components/CroppedImage.js
@@ -29,19 +29,16 @@ const CroppedImage = ({ croppedImage, detected }) => {
                               ></span>
                           ))}
                         </span>
-                                                <span className="dropdown-icon">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down" viewBox="0 0 16 16">
-                            <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
-                          </svg>
-                        </span>
                                             </p>
-                                            <select className="form-control">
-                                                {Object.entries(shapes).map(([key, shape]) => (
-                                                    <option key={key} value={key}>
-                                                        {shape.description}
-                                                    </option>
-                                                ))}
-                                            </select>
+                                            <div className="custom-select-arrow">
+                                                <select className="form-control">
+                                                    {Object.entries(shapes).map(([key, shape]) => (
+                                                        <option key={key} value={key}>
+                                                            {shape.description}
+                                                        </option>
+                                                    ))}
+                                                </select>
+                                            </div>
                                         </div>
                                     </div>
                                 ))}
