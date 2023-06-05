596300122901c1f30cddd8980d4ac5c942d0a359
Elad <eladbendavid7@gmail.com>
2023-06-06 00:38:11 +0300

    Added cropped image logic

--- src/Components/App/components/CroppedImage.js
+++ src/Components/App/components/CroppedImage.js
@@ -6,37 +6,39 @@ const CroppedImage = ({ croppedImage, detected }) => {
     return (
         <div className="container mt-4">
             <h1 className="text-center mb-4">Generated Image</h1>
-            <div className="row justify-content-center">
-                <div className="col-md-6 mb-4">
+            <div className="row">
+                <div className="col-md-12 mb-4">
                     <img src={croppedImage} className="img-fluid" alt="Cropped" />
                 </div>
-                <div className="col-md-6">
+                <div className="col-md-8 offset-md-2">
                     <div className="card">
                         <div className="card-body">
                             <h4 className="card-title">Detected Data</h4>
-                            {Object.entries(detected).map(([label, colors]) => (
-                                <div key={label} className="detected-object">
-                                    <p className="card-text">{label}:</p>
-                                    <div className="detected-info">
-                                        <div className="color-squares">
-                                            {colors.map((color, index) => (
-                                                <div
-                                                    key={index}
-                                                    className="color-square"
-                                                    style={{ backgroundColor: `rgb(${color.join(', ')})` }}
-                                                ></div>
-                                            ))}
+                            <div className="row">
+                                {Object.entries(detected).map(([label, colors], index) => (
+                                    <div key={label} className="col-md-4 mb-4">
+                                        <div className="detected-object">
+                                            <p className="card-text">{label}:</p>
+                                            <div className="color-squares">
+                                                {colors.map((color, index) => (
+                                                    <div
+                                                        key={index}
+                                                        className="color-square"
+                                                        style={{ backgroundColor: `rgb(${color.join(', ')})` }}
+                                                    ></div>
+                                                ))}
+                                            </div>
+                                            <select className="form-control">
+                                                {Object.entries(shapes).map(([key, shape]) => (
+                                                    <option key={key} value={key}>
+                                                        {shape.description}
+                                                    </option>
+                                                ))}
+                                            </select>
                                         </div>
-                                        <select className="form-control">
-                                            {Object.entries(shapes).map(([key, shape]) => (
-                                                <option key={key} value={key}>
-                                                    {shape.description}
-                                                </option>
-                                            ))}
-                                        </select>
                                     </div>
-                                </div>
-                            ))}
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
