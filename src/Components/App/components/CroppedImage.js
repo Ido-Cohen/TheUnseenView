c0c0e8425803d2e4e06c5a222dcb8e90040c20ef
Elad <eladbendavid7@gmail.com>
2023-06-06 00:30:26 +0300

    Added cropped image logic

--- src/Components/App/components/CroppedImage.js
+++ src/Components/App/components/CroppedImage.js
@@ -6,8 +6,8 @@ const CroppedImage = ({ croppedImage, detected }) => {
     return (
         <div className="container mt-4">
             <h1 className="text-center mb-4">Generated Image</h1>
-            <div className="row justify-content-center align-items-center">
-                <div className="col-md-6">
+            <div className="row justify-content-center">
+                <div className="col-md-6 mb-4">
                     <img src={croppedImage} className="img-fluid" alt="Cropped" />
                 </div>
                 <div className="col-md-6">
@@ -15,24 +15,26 @@ const CroppedImage = ({ croppedImage, detected }) => {
                         <div className="card-body">
                             <h4 className="card-title">Detected Data</h4>
                             {Object.entries(detected).map(([label, colors]) => (
-                                <div key={label}>
+                                <div key={label} className="detected-object">
                                     <p className="card-text">{label}:</p>
-                                    <div className="color-squares">
-                                        {colors.map((color, index) => (
-                                            <div
-                                                key={index}
-                                                className="color-square"
-                                                style={{ backgroundColor: `rgb(${color.join(', ')})` }}
-                                            ></div>
-                                        ))}
+                                    <div className="detected-info">
+                                        <div className="color-squares">
+                                            {colors.map((color, index) => (
+                                                <div
+                                                    key={index}
+                                                    className="color-square"
+                                                    style={{ backgroundColor: `rgb(${color.join(', ')})` }}
+                                                ></div>
+                                            ))}
+                                        </div>
+                                        <select className="form-control">
+                                            {Object.entries(shapes).map(([key, shape]) => (
+                                                <option key={key} value={key}>
+                                                    {shape.description}
+                                                </option>
+                                            ))}
+                                        </select>
                                     </div>
-                                    <select className="form-control">
-                                        {Object.entries(shapes).map(([key, shape]) => (
-                                            <option key={key} value={key}>
-                                                {shape.description}
-                                            </option>
-                                        ))}
-                                    </select>
                                 </div>
                             ))}
                         </div>
