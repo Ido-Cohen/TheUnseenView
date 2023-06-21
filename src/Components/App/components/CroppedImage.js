8d23a19bce148c7b6bc72341cc2e87f54836559b
Elad <eladbendavid7@gmail.com>
2023-06-21 19:43:24 +0300

    Fix CroppedImage.js

--- src/Components/App/components/CroppedImage.js
+++ src/Components/App/components/CroppedImage.js
@@ -1,23 +1,35 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import shapes from './constants/shapes';
 import './CroppedImage.css';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import axios from 'axios';
-import { toast } from 'react-toastify';
+import {toast} from 'react-toastify';
 import seedrandom from 'seedrandom';
 
-const CroppedImage = ({ croppedImage, detected, onNext }) => {
+const CroppedImage = ({croppedImage, detected, onNext}) => {
     const [selectedOptions, setSelectedOptions] = useState({});
     const navigate = useNavigate();
 
     const handleOptionChange = (id, value) => {
-        setSelectedOptions((prevOptions) => ({
-            ...prevOptions,
-            [id]: value,
-        }));
+        setSelectedOptions((prevOptions) => {
+            const updatedOptions = { ...prevOptions };
+
+            if (value === '') {
+                delete updatedOptions[id];
+            } else {
+                updatedOptions[id] = value;
+            }
+
+            return updatedOptions;
+        });
     };
 
     const handleNextClick = () => {
+        Object.keys(selectedOptions).forEach((key) => {
+            if (selectedOptions[key] === 'EMPTY') {
+                delete selectedOptions[key];
+            }
+        });
         const jsonContent = JSON.stringify(
             Object.entries(selectedOptions).reduce((result, [label, value]) => {
                 const [r, g, b] = detected[label][0]; // Get the RGB values from detected colors
@@ -29,10 +41,10 @@ const CroppedImage = ({ croppedImage, detected, onNext }) => {
 
         // Make a POST request with Axios
         axios.post('http://theunseenview.org:777/chosenPatterns', jsonContent, {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            })
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
             .then((response) => {
                 // Handle the response from the server
                 console.log('Post request successful:', response.data);
@@ -56,9 +68,7 @@ const CroppedImage = ({ croppedImage, detected, onNext }) => {
 
     const handleRandomizeClick = () => {
         const availableShapes = Object.keys(shapes).filter(
-            (shapeKey) =>
-                shapeKey !== 'EMPTY' &&
-                !Object.values(selectedOptions).includes(shapeKey)
+            (shapeKey) => shapeKey !== 'EMPTY' && !Object.values(selectedOptions).includes(shapeKey)
         );
 
         // Create a random number generator with a fixed seed value
@@ -66,20 +76,27 @@ const CroppedImage = ({ croppedImage, detected, onNext }) => {
         const rng = new seedrandom(currentTimestamp);
 
         const randomizedOptions = { ...selectedOptions };
-
-        Object.keys(detected).forEach((id) => {
+        Object.keys(detected).forEach((id, index) => {
             const selectedShape = randomizedOptions[id];
-            if (!selectedShape && availableShapes.length > 0) {
-                const randomIndex = Math.floor(rng() * availableShapes.length);
-                const shapeToAssign = availableShapes[randomIndex];
-                randomizedOptions[id] = shapeToAssign;
-                availableShapes.splice(randomIndex, 1);
-            } else if (!selectedShape && availableShapes.length === 0) {
-                randomizedOptions[id] = 'EMPTY';
+            console.log(selectedShape);
+            if (!selectedShape || selectedShape === 'EMPTY') {
+                if (index < 6 && availableShapes.length > 0) {
+                    const randomIndex = Math.floor(rng() * availableShapes.length);
+                    const shapeToAssign = availableShapes[randomIndex];
+                    randomizedOptions[id] = shapeToAssign;
+                    availableShapes.splice(randomIndex, 1);
+                } else {
+                    randomizedOptions[id] = 'EMPTY';
+                }
             }
         });
 
         setSelectedOptions(randomizedOptions);
+    };
+
+    const isDisabled = (selectedOptions) => {
+        const nonEmptyOptions = Object.values(selectedOptions).filter(option => option !== 'EMPTY');
+        return nonEmptyOptions.length >= 6;
     };
 
     return (
@@ -94,20 +111,20 @@ const CroppedImage = ({ croppedImage, detected, onNext }) => {
                         <div className="card-body">
                             <h4 className="card-title">Detected Data</h4>
                             <div className="row">
-                                {Object.entries(detected).map(([label, colors]) => (
+                                {Object.entries(detected).map(([label, colors], index) => (
                                     <div key={label} className="col-md-4 mb-4">
                                         <div className="detected-object">
                                             <p className="card-text">
                                                 <span className="object-name">{label}:</span>
                                                 <span className="color-squares">
-                          {colors.map((color, index) => (
-                              <span
-                                  key={index}
-                                  className="color-square"
-                                  style={{ backgroundColor: `rgb(${color.join(', ')})` }}
-                              ></span>
-                          ))}
-                        </span>
+                      {colors.map((color, colorIndex) => (
+                          <span
+                              key={colorIndex}
+                              className="color-square"
+                              style={{ backgroundColor: `rgb(${color.join(', ')})` }}
+                          ></span>
+                      ))}
+                    </span>
                                             </p>
                                             <div className="custom-select-arrow">
                                                 <select
@@ -115,6 +132,7 @@ const CroppedImage = ({ croppedImage, detected, onNext }) => {
                                                     id={label}
                                                     value={selectedOptions[label] || ''}
                                                     onChange={(e) => handleOptionChange(label, e.target.value)}
+                                                    disabled={isDisabled(selectedOptions) && (selectedOptions[label] === 'EMPTY' || !selectedOptions[label])}
                                                 >
                                                     <option value="">Select an option</option>
                                                     {Object.entries(shapes).map(([key, shape]) => (
@@ -140,7 +158,9 @@ const CroppedImage = ({ croppedImage, detected, onNext }) => {
                                     onClick={handleNextClick}
                                     disabled={isNextButtonDisabled}
                                     title={
-                                        isNextButtonDisabled ? 'You have to select all objects' : 'Click to proceed to the next step'
+                                        isNextButtonDisabled
+                                            ? 'You have to select all objects'
+                                            : 'Click to proceed to the next step'
                                     }
                                 >
                                     Next
